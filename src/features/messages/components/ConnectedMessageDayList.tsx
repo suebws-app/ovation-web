b35ee8e089f3eeec4005d0ec0ece397ae4472fa1
Arian Nimani <aarriiaann@gmail.com>
2026-04-28 19:42:19 +0200

    feat(messages): add ConnectedMessageDayList wrapper

---
+++ src/features/messages/components/ConnectedMessageDayList.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import type { AudioPlayer } from "@ovation/ui/hooks/useAudioPlayer";
+import {
+  useActiveMessageId,
+  useMessagesStore,
+  useSelectedIds,
+} from "../store/useMessagesStore";
+import { useMessageList } from "../hooks/useMessageList";
+import { useResponsiveRowOpen } from "../hooks/useResponsiveRowOpen";
+import { MessageDayList } from "./MessageDayList";
+
+type Props = {
+  player: AudioPlayer;
+};
+
+export const ConnectedMessageDayList = ({ player }: Props) => {
+  const { messageViews } = useMessageList();
+  const selectedIds = useSelectedIds();
+  const activeMessageId = useActiveMessageId();
+  const toggleSelected = useMessagesStore((s) => s.toggleSelected);
+  const { openRow, playRow } = useResponsiveRowOpen(player);
+
+  return (
+    <MessageDayList
+      messages={messageViews}
+      selectedIds={selectedIds}
+      activeMessageId={activeMessageId}
+      playingId={player.playingId}
+      isPlaying={player.isPlaying}
+      playingProgress={player.progress}
+      playingDuration={player.duration}
+      playingCurrentTime={player.currentTime}
+      onRowClick={openRow}
+      onRowPlay={playRow}
+      onRowToggleSelect={toggleSelected}
+    />
+  );
+};
